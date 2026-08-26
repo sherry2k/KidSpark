1d04fb706747de93a7331a6599514cce8cb3a79b
Sherry <131241296+sherry2k@users.noreply.github.com>
2026-08-26 23:01:34 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ContentProvider } from "./context/ContentContext";
 import './styles/kidspark-type.css';
+import { ContentProvider } from "./context/ContentContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
